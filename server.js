08ba082fe6295af0272de632f243d3b7a71b2f56
TechMe103 <salunkhesanika610@gmail.com>
2025-08-10 05:52:37 -0700

    made changes

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 const connectDB = require('./config/db');
+require("dotenv").config();
 const PORT = 5000;
 
 const app = express();
 
 
 connectDB()
-  .then(() => console.log("Database connected"))
-  .catch(err => {
-    console.error("Database connection failed", err);
-    process.exit(1);
-  });
-
-
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 
 // Routes
 app.use("/students", require('./routes/studentRoutes'));
+app.use("/internships" , require("./routes/internRoutes"));
 app.use("/achievements", require("./routes/achievementRoutes"));
 app.use("/activities", require("./routes/activitiesRoutes"));
 app.use("/semesterInfo", require("./routes/semInfoRoutes"));
 
+
+app.get("/" , (req, res) => {
+  res.send("API is running...");
+});
 
 
 app.listen(PORT, () => {
