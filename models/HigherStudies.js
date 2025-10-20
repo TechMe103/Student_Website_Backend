5b425e09dadb1947dfe275f1f08fe1c16cb74b77
AasthaOswal <aasthaoswal29@gmail.com>
2025-10-20 09:33:41 +0530

    Added model for HigherStudies.js

---
+++ models/HigherStudies.js
@@ -0,0 +1,29 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+
+const higherStudiesSchema = new mongoose.Schema({
+	stuID: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Student",
+		required: true,
+	},
+
+	examName: {
+		type: String,
+		enum: ["GATE", "CAT", "GRE", "TOFEL", "IELTS", "UPSC"],
+		required: true,
+	},
+
+	score: {
+		type: Number,
+		required: true,
+	},
+
+	marksheet: {
+		url: { type: String, required:true },      // Cloudinary secure URL
+		publicId: { type: String , required:true}, // Cloudinary public_id for deletion
+	},
+},
+{ timestamps: true });
+
+module.exports = mongoose.model("HigherStudies", higherStudiesSchema);
