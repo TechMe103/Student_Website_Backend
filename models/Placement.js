7a57471e4ca85b41b95b0df749613f31206a67af
AasthaOswal <aasthaoswal29@gmail.com>
2025-10-20 08:42:13 +0530

    Made placementProof compulsory by adding required:true

--- models/Placement.js
+++ models/Placement.js
@@ -1,32 +1,33 @@
+const { required } = require("joi");
 const mongoose = require("mongoose");
 
 const placementSchema = new mongoose.Schema({
-  stuID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Student",
-    required: true,
-  },
+	stuID: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Student",
+		required: true,
+	},
 
-  companyName: {
-    type: String,
-    required: true,
-  },
+	companyName: {
+		type: String,
+		required: true,
+	},
 
-  role: {
-    type: String,
-    required: true,
-  },
+	role: {
+		type: String,
+		required: true,
+	},
 
-  placementType: {
-    type: String,
-    enum: ["Campus", "Off-Campus"],
-    required: true,
-  },
+	placementType: {
+		type: String,
+		enum: ["Campus", "Off-Campus"],
+		required: true,
+	},
 
-  placementProof: {
-    url: { type: String },      // Cloudinary secure URL
-    publicId: { type: String }, // Cloudinary public_id for deletion
-  },
+	placementProof: {
+		url: { type: String, required : true },      // Cloudinary secure URL
+		publicId: { type: String, required : true }, // Cloudinary public_id for deletion
+	},
 },
 { timestamps: true });
 
