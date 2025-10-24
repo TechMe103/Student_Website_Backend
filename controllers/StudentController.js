afcb1b0065024ab3b0994c1925758e5ea245c03f
AasthaOswal <aasthaoswal29@gmail.com>
2025-10-24 19:00:23 +0530

    Fixed small issue in delete controller

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -417,8 +417,11 @@ const deleteStudent = async (req, res) => {
 		// Delete student photo from Cloudinary if exists
 		if (student.studentPhoto.publicId) {
 			const delResult=await cloudinary.uploader.destroy(student.studentPhoto.publicId);
+
+			console.log("Cloudinary delete result:", delResult);
+
 			
-			if(delResult.result !== "ok"){
+			if(delResult.result !== "ok" && delResult.result !== "not found"){
 				return res.status(500).json({ success: false, message: "Cannot delete student. Please try again later." });
 			}
 		}
