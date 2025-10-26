8a5287e3064ea0730593dcba688d51b65daba591
sanika Salunkhe <salunkhesanika610@gmail.com>
2025-10-25 19:20:09 -0700

    Simplify CORS configuration and update routes
    Signed-off-by: sanika Salunkhe <salunkhesanika610@gmail.com>

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const app = express();
 connectDB()
 
 // Middlewares
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials : true,
-}));
+app.use(cors());
 app.use(express.json({limit: "50mb",}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
 app.use(cookieParser());
@@ -29,16 +26,17 @@ app.use("/api/internship" , require("./routes/internRoutes"));
 app.use("/api/achievements", require("./routes/achievementRoutes"));
 app.use("/api/activities", require("./routes/activitiesRoutes"));
 app.use("/api/semesterInfo", require("./routes/semInfoRoutes"));
+app.use("/api/admission", require("./routes/admissionRoutes"));
 app.use("/api/placement", require("./routes/PlacementRoutes"));
 app.use("/api/higherStudies", require("./routes/HigherStudiesRoutes"));
 app.use("/api/student", require("./routes/StudentRoutes"));
 
 
 app.get("/" , (req, res) => {
-  	res.send("API is running...");
+  res.send("API is running...");
 });
 
 
 app.listen(PORT, () => {
-  	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
