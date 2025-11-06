7c5bdbd8706a7c02ecfb929948336e604214c471
sanika Salunkhe <salunkhesanika610@gmail.com>
2025-11-06 01:32:01 -0800

    Update activitiesValidation.js
    Signed-off-by: sanika Salunkhe <salunkhesanika610@gmail.com>

--- validators/activitiesValidation.js
+++ validators/activitiesValidation.js
@@ -2,16 +2,15 @@ const Joi = require("joi");
 
 const activitySchema = Joi.object({
   type: Joi.string()
-    .valid("Committee", "Sports", "Hackathon")
+    .valid("Committee") //Only Committee allowed
     .required(),
   title: Joi.string().min(3).max(100).required(),
   description: Joi.string().min(5).max(500).required(),
   date: Joi.date().required(),
   certificateURL: Joi.object({
-  url: Joi.string().uri().required(),
-  publicId: Joi.string().optional(),
-}).optional()
-
+    url: Joi.string().uri().required(),
+    publicId: Joi.string().optional(),
+  }).optional(),
 });
 
 module.exports = { activitySchema };
