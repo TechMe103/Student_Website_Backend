bb607c1db53d457e2a5bf72667e618cfcd3f2a1e
AasthaOswal <aasthaoswal29@gmail.com>
2025-12-02 19:28:37 +0530

    Finalised Placement model and validator

--- validators/placementValidation.js
+++ validators/placementValidation.js
@@ -1,34 +1,3 @@
-// const Joi = require("joi");
-
-// const createPlacementSchema = Joi.object({
-//     companyName: Joi.string().trim().required(),
-
-//     role: Joi.string().trim().required(),
-
-//     placementType: Joi.string().valid("Campus", "Off-Campus").required(),
-
-// });
-
-// const updatePlacementSchema = Joi.object({
-//     companyName: Joi.string().trim(),
-
-//     role: Joi.string().trim(),
-
-//     placementType: Joi.string().valid("Campus", "Off-Campus"),
-
-// });
-
-// const getPlacementsValidation = Joi.object({
-//   year: Joi.string().valid("SE", "TE", "BE").optional(),
-//   placementType: Joi.string().valid("Campus", "Off-Campus").optional(),
-//   search: Joi.string().max(100).optional(),
-//   page: Joi.number().integer().min(1).optional(),
-//   limit: Joi.number().integer().min(1).max(20).optional()
-// });
-
-// module.exports = {createPlacementSchema, updatePlacementSchema, getPlacementsValidation};
-
-
 const Joi = require("joi");
 
 
@@ -45,7 +14,7 @@ const createPlacementSchema = Joi.object({
 
 	placementType: Joi.string().valid("Campus", "Off-Campus").required(),
 
-	package: Joi.number().positive().min(1).required(),
+	package: Joi.number().positive().min(1).max(100).required(),
 
 	placementYear: Joi.string().min(1).pattern(yearPattern).required(),
 
@@ -64,7 +33,7 @@ const updatePlacementSchema = Joi.object({
 
 	placementType: Joi.string().valid("Campus", "Off-Campus").empty("").optional(),
 
-	package: Joi.number().positive().min(1).empty("").optional(),
+	package: Joi.number().positive().min(1).max(100).empty("").optional(),
 
 	placementYear: Joi.string().pattern(yearPattern).empty("").optional(),
 
